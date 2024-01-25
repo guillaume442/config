0bbfb395669b4ee24d8bafc527778e9e1572f450
Thomas Brandt <soey@SoeyTCG-2.local>
2024-01-25 14:15:31 +0100

    fichier conf

---
+++ index.php
@@ -0,0 +1,82 @@
+<?php
+$host = "localhost"; // Remplacez par l'hôte de votre base de données
+$dbname = "afci"; // Remplacez par le nom de votre base de données
+$user = "root"; // Remplacez par votre nom d'utilisateur
+$pass = ""; // Remplacez par votre mot de passe
+
+
+    // Création d'une nouvelle instance de la classe PDO
+    $bdd = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $user, $pass);
+
+    // Configuration des options PDO
+    $bdd->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $bdd->setAttribute(PDO::ATTR_EMULATE_PREPARES, false);
+
+    // echo "Connexion réussie !";
+
+    // Lire des données dans la BDD
+    $sql = "SELECT * FROM apprenants";
+    $requete = $bdd->query($sql);
+    $results = $requete->fetchAll(PDO::FETCH_ASSOC);
+    
+
+    // foreach( $results as $value ){
+    //     foreach($value as $data){
+    //         echo $data;
+    //         echo "<br>";
+
+    //     }
+    //     echo "<br>";
+    // }
+
+    foreach( $results as $value ){
+        echo "<h2>" . $value["nom_apprenant"] . "</h2>";
+        echo "<br>";
+    }
+
+
+    // Insérer des données dans la BDD
+
+?>
+
+<form method="POST">
+    <input type="text" name="nomRole">
+    <input type="submit" name="submitRole">
+
+</form>
+
+<?php 
+    if (isset($_POST['submitRole'])){
+        $nomRole = $_POST['nomRole'];
+
+        $sql = "INSERT INTO `role`(`nom_role`) VALUES ('$nomRole')";
+        $bdd->query($sql);
+
+        echo "data ajoutée dans la bdd";
+
+    }
+
+?>
+
+
+<form method="POST">
+    <h1>Ajout Centre</h1>
+    <input type="text" name="nomCentre">
+    <input type="text" name="adresse">
+    <input type="text" name="nomCentre">
+    <input type="submit" name="submitCentre">
+
+</form>
+
+<?php 
+    if (isset($_POST['submitRole'])){
+        $nomRole = $_POST['nomRole'];
+
+        $sql = "INSERT INTO `role`(`nom_role`) VALUES ('$nomRole')";
+        $bdd->query($sql);
+
+        echo "data ajoutée dans la bdd";
+
+    }
+
+?>
