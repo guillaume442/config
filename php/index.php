1ba54b3e2cd528475ccd26bd31312c79bd74b071
Thomas Brandt <soey@SoeyTCG-2.local>
2024-01-30 09:15:42 +0100

    update pedagogie

--- php/index.php
+++ php/index.php
@@ -1,3 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>AFCI - Gestion des centres</title>
+</head>
+<body>
+    <header>
+        <nav>
+            <ul>
+                <a href="?page=role"><li>Role</li></a>
+                <a href="?page=centre"><li>Centre</li></a>
+                <a href="?page=formation"><li>Formation</li></a>
+                <a href="?page=pedagogie"><li>Pedagogie</li></a>
+                <a href="?page=session"><li>Session</li></a>
+                <a href="?page=apprenant"><li>Apprenants</li></a>
+            </ul>
+        </nav>
+    </header>
+
+
 <?php
 $host = "mysql"; // Remplacez par l'hôte de votre base de données
 $port = "3306";
@@ -37,24 +59,201 @@ $pass = "admin"; // Remplacez par votre mot de passe
 
 
     // Insérer des données dans la BDD
+// Gestion de la page role
+if (isset($_GET["page"]) && $_GET["page"] == "role"){
+ ?>
+    <form method="POST">
+        <h1>Ajout d'un role</h1>
+        <label for="">Nom du role</label>
+        <input type="text" name="nomRole">
+        <input type="submit" name="submitRole" value="enregistrer">
+    
+    </form>
 
-?>
+    
+    
+<?php
 
-<form method="POST">
-    <input type="text" name="nomRole">
-    <input type="submit" name="submitRole">
+    $sql = "SELECT * FROM role";
+    $requete = $bdd->query($sql);
+    $results = $requete->fetchAll(PDO::FETCH_ASSOC);
 
-</form>
+    foreach( $results as $value ){
+        foreach($value as $data){
+            echo $data;
+            echo "<br>";
 
-<?php 
-    if (isset($_POST['submitRole'])){
-        $nomRole = $_POST['nomRole'];
-
-        $sql = "INSERT INTO `role`(`nom_role`) VALUES ('$nomRole')";
-        $bdd->query($sql);
-
-        echo "data ajoutée dans la bdd";
-
+        }
+        echo "<br>";
     }
 
+}
+
+if (isset($_POST['submitRole'])){
+    $nomRole = $_POST['nomRole'];
+
+    $sql = "INSERT INTO `role`(`nom_role`) VALUES ('$nomRole')";
+    $bdd->query($sql);
+
+    echo "data ajoutée dans la bdd";
+
+}
+
+
+// Gestion de la page centre
+if (isset($_GET["page"]) && $_GET["page"] == "centre"){
+    ?>
+       <form method="POST">
+            <h1>Ajout d'un centre</h1>
+            <label>Nom de la ville</label>
+           <input type="text" name="villeCentre">
+           <label for="">Adresse</label>
+           <input type="text" name="adresseCentre">
+           <label for="">Code postal</label>
+           <input type="text" name="cpCentre">
+           <input type="submit" name="submitCentre" value="enregistrer">
+       
+       </form>
+       
+   <?php
+
+        $sql = "SELECT * FROM centres";
+        $requete = $bdd->query($sql);
+        $results = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+        foreach( $results as $value ){
+        foreach($value as $data){
+            echo $data;
+            echo "<br>";
+
+        }
+        echo "<br>";
+        }
+   }
+
+   if (isset($_POST['submitCentre'])){
+    $villeCentre = $_POST['villeCentre'];
+    $adresseCentre = $_POST['adresseCentre'];
+    $cpCentre = $_POST['cpCentre'];
+
+    $sql = "INSERT INTO `centres`(`ville_centre`, `adresse_centre`, `code_postal_centre`) VALUES ('$villeCentre','$adresseCentre','$cpCentre')";
+    $bdd->query($sql);
+
+    echo "data ajoutée dans la bdd";
+
+}
+
+// gestion de la page Formation
+   if (isset($_GET["page"]) && $_GET["page"] == "formation"){
+    ?>
+       <form method="POST">
+       <h1>Ajout d'une formation</h1>
+           <input type="text" name="nomRole">
+           <input type="submit" name="submitRole" value="enregistrer">
+       
+       </form>
+       
+   <?php
+   }
+
+// Gestion de la page Pedagogie
+   if (isset($_GET["page"]) && $_GET["page"] == "pedagogie"){
+
+    $sql = "SELECT * FROM role";
+    $requete = $bdd->query($sql);
+    $results = $requete->fetchAll(PDO::FETCH_ASSOC);
+
+
+    ?>
+       <form method="POST">
+       <h1>Ajout d'un membre de l'équipe pédagogique</h1>
+            <label>Nom</label>
+            <input type="text" name="nomPedagogie">
+            <label>Prénom</label>
+            <input type="text" name="prenomPedagogie">
+            <label>Adresse mail</label>
+            <input type="text" name="numPedagogie">
+            <label>Numéro</label>
+            <input type="text" name="mailPedagogie">
+            <label>Rôle</label>
+            <select name="idPedagogie" id="">
+                <!-- <option value="idrole">id - nom role</option> -->
+                <?php 
+                
+                foreach( $results as $value ){             
+                        echo '<option value="' . $value['id_role'] .  '">' . $value['id_role'] . ' - ' . $value['nom_role'] . '</option>';   
+                }
+                ?>
+            </select>
+            <input type="submit" name="submitPedagogie" value="enregistrer">
+       
+       </form>
+       
+   <?php
+
+$sql = "SELECT * FROM pedagogie";
+    $requete = $bdd->query($sql);
+    $results = $requete->fetchAll(PDO::FETCH_ASSOC);
+    
+
+    foreach( $results as $value ){
+        foreach($value as $data){
+            echo $data;
+            echo "<br>";
+
+        }
+        echo "<br>";
+    }
+
+   }
+
+   if (isset($_POST['submitPedagogie'])){
+    $nomPedagogie = $_POST['nomPedagogie'];
+    $prenomPedagogie = $_POST['prenomPedagogie'];
+    $numPedagogie = $_POST['numPedagogie'];
+    $mailPedagogie = $_POST['mailPedagogie'];
+    $idPedagogie = $_POST['idPedagogie'];
+
+    $sql = "INSERT INTO `pedagogie`(`nom_pedagogie`, `prenom_pedagogie`, `mail_pedagogie`, `num_pedagogie`, `id_role`) VALUES ('$nomPedagogie','$prenomPedagogie','$numPedagogie','$mailPedagogie','$idPedagogie')";
+    $bdd->query($sql);
+
+    echo "data ajoutée dans la bdd";
+
+}
+
+// Gestion de la page session
+   if (isset($_GET["page"]) && $_GET["page"] == "session"){
+    ?>
+       <form method="POST">
+       <h1>Ajout d'une session</h1>
+           <input type="text" name="nomRole">
+           <input type="submit" name="submitRole" value="enregistrer">
+       
+       </form>
+       
+   <?php
+   }
+
+// Gestion de la page apprenant
+   if (isset($_GET["page"]) && $_GET["page"] == "apprenant"){
+    ?>
+       <form method="POST">
+       <h1>Ajout d'un apprenant</h1>
+           <input type="text" name="nomRole">
+           <input type="submit" name="submitRole" value="enregistrer">
+       
+       </form>
+       
+   <?php
+   }
+
 ?>
+
+
+
+<?php 
+
+
+?>
+</body>
+</html>
